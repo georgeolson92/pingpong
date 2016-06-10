453e05c33f54f6a0518e40fd32524597668df511
George Olson <georgeolson@Georges-MacBook-Air.local>
2016-06-10 08:58:38 -0700

    Add function that displays 'ping', 'pong' or 'pingpong' depending on whether the entered number is divisible by 3,5,or 15 respectively

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // Back End
 
 function isDivisible (inputNumber){
-  return inputNumber;
+  if ((inputNumber % 15) === 0){
+    return "pingpong"
+  } else if ((inputNumber % 5) === 0){
+    return "pong";
+  } else if ((inputNumber % 3) === 0){
+    return "ping";
+  } else {
+    return inputNumber;
+  }
 }
 
 
